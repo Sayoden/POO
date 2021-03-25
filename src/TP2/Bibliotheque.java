bae05090c1228e4f387e26db135caa9d822b5400
Tinwynn <stanwisepala@gmail.com>
2021-03-25 23:13:16 +0100

    Update some class..

--- src/TP2/Bibliotheque.java
+++ src/TP2/Bibliotheque.java
@@ -1,5 +1,8 @@
 package TP2;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Bibliotheque {
 
     private String nom;
@@ -7,26 +10,103 @@ public class Bibliotheque {
     private int nbLivres;
     private int nbExemplaires;
 
-    final static int MAX_LIVRES = 100;
+    final static int MAX_LIVRE = 100;
     final static int MAX_EXEMPLAIRES = 1000;
+
 
     public Bibliotheque(String nom) {
         this.nom = nom;
-        this.livres = new Livre[MAX_LIVRES];
+        this.livres = new Livre[MAX_LIVRE];
         this.nbLivres = 0;
         this.nbExemplaires = 0;
     }
 
     public void addLivre(Livre livre){
-
         if(nbLivres == 0){
             livres[0] = livre;
             nbLivres++;
             return;
         }
 
+        if((livre.getExemplaires() + nbExemplaires) > MAX_EXEMPLAIRES){
+            System.out.println("Il y a déjà trop d'exemplaire dans cette bibliothèque.");
+            return;
+        }
+
+        for(int i = 0; i < nbLivres; i++){
+            System.out.println(livres[i].getReference());
+            if(livres[i].getReference().equals(livre.getReference())){
+                System.out.println("Livre avec la même reference trouvé, abandon.");
+                return;
+            }
+        }
+
+        System.out.println("La réference n'a pas été trouvé, implémentation du livre dans la bibliothèque");
         livres[nbLivres] = livre;
+        nbLivres++;
     }
 
+    public void addOneExemplaire(String reference){
+        if(nbExemplaires + 1 <= MAX_EXEMPLAIRES){
+            for(int i = 0; i < nbLivres; i++){
+                if(livres[i].getReference().equals(reference)){
+                    System.out.println("Livre avec la reference trouvé, ajout d'un exemplaire");
+                    livres[i].addOneExemplaire();
+                    return;
+                }
+            }
+        }else System.out.println("Il y a déjà trop d'exemplaire dans cette bibliothèque");
 
+        System.out.println("La reference n'a pas été trouvé");
+    }
+
+    public void addMultiplesExemplaires(String reference, int nb){
+        if(nbExemplaires + nb <= MAX_EXEMPLAIRES){
+            for(int i = 0; i < nbLivres; i++){
+                if(livres[i].getReference().equals(reference)){
+                    System.out.println("Livre avec la reference trouvé, ajout d'un exemplaire");
+                    livres[i].addMultiplesExemplaires(nb);
+                    return;
+                }
+            }
+        }else System.out.println("Il y a déjà trop d'exemplaire dans cette bibliothèque");
+
+        System.out.println("La reference n'a pas été trouvé");
+    }
+
+    public void removeLivre(String reference){
+        int emplacement;
+        for(int i = 0; i < nbLivres; i++){
+            if(livres[i].getReference().equals(reference)){
+                emplacement = i;
+                for(int y = emplacement; y < nbLivres; y++){
+                    livres[i] = livres[i + 1];
+                }
+                nbLivres--;
+                return;
+            }
+        }
+    }
+
+    public void display(){
+        System.out.println("Nombre de livre: "+nbLivres);
+        for(int i = 0; i < nbLivres; i++){
+            System.out.println(livres[i].getTitre());
+        }
+    }
+
+    public void removeExemplaire(String reference){
+        for(int i = 0; i < nbLivres; i++){
+            if(livres[i].getReference().equals(reference)){
+                if(livres[i].getExemplaires() <= 0 || (livres[i].getExemplaires() - 1) <= 0){
+                    removeLivre(reference);
+                    System.out.println("Le livre a été supprimé car il n'y avait plus d'exemplaire");
+                }else{
+                    livres[i].removeExemplaire();
+                }
+                return;
+            }
+        }
+    }
 }
+
--- src/TP2/Livre.java
+++ src/TP2/Livre.java
@@ -2,14 +2,81 @@ package TP2;
 
 public class Livre {
 
-    private String reference, titre;
+    public enum Genre{
+        FRANCAISE,
+        ETRANGERE,
+        JEUNESSE,
+        SCIENCE_FICTION,
+        POLICE
+    }
 
-    public Livre(String reference, String titre) {
+    private Genre genre;
+    private String reference, titre, editeur;
+    private int exemplaires;
+    private Auteur auteur;
+
+    public Livre(String reference, String titre, Auteur auteur, String editeur, int exemplaires, Genre genre) {
         this.reference = reference;
         this.titre = titre;
+        this.auteur = auteur;
+        this.editeur = editeur;
+        this.exemplaires = exemplaires;
+        this.genre = genre;
     }
 
-    public int getExemplaire(){
-        return 100;
+    public static void main(String[] args){
+
     }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public Auteur getAuteur() {
+        return auteur;
+    }
+
+    public String getEditeur() {
+        return editeur;
+    }
+
+    public int getExemplaires() {
+        if(exemplaires < 0){
+            exemplaires = 0;
+        }
+        return exemplaires;
+    }
+
+    public Genre getGenre(){
+        return genre;
+    }
+
+    public void setEditeur(String editeur) {
+        this.editeur = editeur;
+    }
+
+    public void addOneExemplaire(){
+        exemplaires++;
+    }
+
+    public void addMultiplesExemplaires(int nb){
+        if(nb <= 0) return;
+        exemplaires = exemplaires + nb;
+    }
+
+    public void removeExemplaire(){
+        if(exemplaires <= 0) return;
+        exemplaires--;
+    }
+
+    public boolean isPresent(){
+        if(exemplaires <= 0) return false;
+        return true;
+    }
+
+
 }
