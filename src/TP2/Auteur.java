fb33eb980877e024df85cd2231b59ae3ad5f1412
Tinwynn <stanwisepala@gmail.com>
2021-03-25 23:09:55 +0100

    New auteur class

--- src/TP2/Auteur.java
+++ src/TP2/Auteur.java
@@ -2,27 +2,26 @@ package TP2;
 
 public class Auteur {
 
-    private String reference, nom, prenom, nationalité;
+    private String reference, nom, prenom, nationalite;
     private int age;
 
-    public Auteur(String reference, String nom, String prenom, String nationalité, int age) {
+    public Auteur(String reference, String nom, String prenom, String nationalite, int age) {
         this.reference = reference;
         this.nom = nom;
         this.prenom = prenom;
-        this.nationalité = nationalité;
+        this.nationalite = nationalite;
         this.age = age;
     }
 
     public boolean equals(String reference){
-        if (reference == this.reference) return true;
-        return false;
+        return reference.equals(this.reference);
     }
 
     public String toString() {
         return reference+" "+
                 nom+" "+
                 prenom+" "+
-                nationalité+" "+
+                nationalite+" "+
                 age;
     }
 
@@ -42,8 +41,8 @@ public class Auteur {
         return prenom;
     }
 
-    public String getNationalité() {
-        return nationalité;
+    public String getNationalite() {
+        return nationalite;
     }
 
     public int getAge() {
