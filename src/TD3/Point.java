13ce64d2b9a18f2ab9338d33b35c7f8cb931b79b
Lucas <66736146+Sayoden@users.noreply.github.com>
2021-04-06 12:19:26 +0200

    Add TD3

---
+++ src/TD3/Point.java
@@ -0,0 +1,17 @@
+package TD3;
+
+public class Point {
+
+    private double x,y;
+
+    public Point(double v, double v1) {
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
